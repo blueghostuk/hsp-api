c02747d1adc70429d55a478f9bcc88f7f19e9745
Michael Pritchard <blueghostuk@gmail.com>
2016-07-23 16:22:28 +0100

    update controller classes

--- Controllers/DelaysController.cs
+++ Controllers/DelaysController.cs
@@ -1,13 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using Microsoft.Extensions.Options;
 using System.Net.Http.Headers;
 using hsp_api.Model;
-using System.Net;
+using Newtonsoft.Json;
 
 namespace hsp_api.Controllers
 {
@@ -21,9 +19,8 @@ namespace hsp_api.Controllers
             _settings = settings.Value;
         }
 
-        // GET api/values
         [HttpGet]
-        public async Task<string> Get(string fromCRS, string toCRS, DateTime? startDate = null, DateTime? endDate = null)
+        public async Task<ServiceMetricsResponse> Get(string fromCRS, string toCRS, DateTime? startDate = null, DateTime? endDate = null)
         {
             startDate = startDate ?? DateTime.UtcNow.AddHours(-2);
             endDate = endDate ?? DateTime.UtcNow;
@@ -45,13 +42,37 @@ namespace hsp_api.Controllers
                 ));
                 if (response.IsSuccessStatusCode)
                 {
-                    return await response.Content.ReadAsStringAsync();
-                    //var responseJson = await response.Content.ReadAsAsync<string[]>();
-                    //do something with the response here. Typically use JSON.net to deserialise it and work with it
+                    var content = await response.Content.ReadAsStringAsync();                    
+                    return JsonConvert.DeserializeObject<ServiceMetricsResponse>(content);
                 }
             }
 
-            return string.Empty;
+            return null;
+        }
+
+        [HttpGet]
+        [Route("service")]
+        public async Task<ServiceDetailsResponse> GetServiceDetails(string rid)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_settings.Uri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.AcceptEncoding.Clear();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                    "Basic",
+                    Convert.ToBase64String(
+                        System.Text.ASCIIEncoding.ASCII.GetBytes(
+                        string.Format("{0}:{1}", _settings.Username, _settings.Password))));
+                var response = await client.PostAsJsonAsync<ServiceDetailsRequest>("api/v1/serviceDetails/", new ServiceDetailsRequest(rid));
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();                    
+                    return JsonConvert.DeserializeObject<ServiceDetailsResponse>(content);
+                }
+            }
+
+            return null;
         }
     }
 }
