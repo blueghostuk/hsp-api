606f4d74a39b2117f4adf96f9fd93e33c012c6e4
Michael Pritchard <blueghostuk@gmail.com>
2016-07-23 14:44:24 +0100

    update app settings to have app specific settings

---
+++ Model/Configuration.cs
@@ -0,0 +1,10 @@
+
+namespace hsp_api.Model
+{
+    public sealed class Configuration
+    {
+        public string Uri { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
